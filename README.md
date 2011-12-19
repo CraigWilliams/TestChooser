f89ca0994176e84b9644b7ba07676ac4ebd91317
Craig Williams <cwilliams@allancraig.net>
2011-12-18 17:31:57 -0700

    Switched to straight AppleScript instead of py-appscript for easier installation.

--- README.md
+++ README.md
@@ -1,30 +1,17 @@
 #TestChooser
 A Sublime Text 2 plugin for quickly executing a RSpec & Cucumber commands in [iTerm](http://iterm.sourceforge.net/) or Apple's Terminal.
 
-Since this plugin uses [py-appscript](http://appscript.sourceforge.net/py-appscript/index.html), an AppleScript bridge for Python, it is only usable on OS X.
 
 ## Description
 It first checks the current file to see that it is either a "feature" or "spec" file. If so, it reads the file looking for "it", "describe", "context" or "scenario" keywords that are at the beginning of a line.
 
-It then displays those lines for you to choose from. Once you select an item from the list and click "Run", the test is executed in [iTerm](http://iterm.sourceforge.net/) or Apple's Terminal, whichever you have set in preferences.
+It then displays those lines for you to choose from. Once you select an item from the list and click "Run", the test is executed in [iTerm](http://iterm.sourceforge.net/) or Apple's Terminal, whichever you have set in preferences. The default is iTerm.
 
-On each successive run of the plugin, the previously chosen command will automatically be selected for convenience.
 
 ## Instructions
-#### Install py-appscript
-Sublime Text 2 uses its own Python and its version is 2.6. We need to install appscript with the same version that Sublime is using.
-
-`sudo easy_install-2.6 appscript`
-
-This will create "appscript-1.0.0-py2.6-macosx-10.7-intel.egg" file in /Library/Python/2.6/site-packages
-
-In Sublime, choose "Preferences"->"Browse Packages"
-
-Copy the .egg file into the "Installed Packages" folder and restart Sublime.
-
 #### This Plugin
 Install this plugin using [Package Control](http://wbond.net/sublime_packages/package_control), a
-package manager for Sublime.
+package manager for Sublime Text 2.
 
 #### Key Bindings
 There is a menu item to access the key binding for TestChooser.
@@ -37,11 +24,9 @@ This one uses a two-key combination. The "super" is the "command" key on a Mac.
 
 
 #### Settings
-There are two settings available.
-
 "Preferences"->"Package Settings"->"TestChooser"->"Settings - User"
 
-This defaults to iTerm but you can also choose Apple's Terminal by using the word "Terminal"
+Choose your terminal. The default is iTerm but you can also choose Apple's Terminal by using the word "Terminal"
 
 `"terminal": "iTerm"`
 
@@ -49,6 +34,13 @@ The search terms used when searching the RSpec and Cucumber files.
 
 `"search_terms": ["it", "describe", "context", "Scenario"]`
 
+Set the RSpec terminal command.
+
+`"rspec_command": "bundle exec rspec --drb"`
+
+Set the Cucumber terminal command.
+
+`"cucumber_command": "bundle exec cucumber --drb -r features/"`
 
 ## License
 
--- test_chooser.py
+++ test_chooser.py
@@ -1,120 +1,87 @@
-try:
-  from appscript import *
-  from osax import *
-except ImportError, e:
-  pass
-
-import os
+import os, subprocess
 import sublime, sublime_plugin, io
 
 class TestChooserCommand(sublime_plugin.WindowCommand):
 
   def run(self, paths = [], name = ""):
+    self.settings         = sublime.load_settings('TestChooser.sublime-settings')
+    search_terms          = self.settings.get('search_terms')
+    self.terminal         = self.settings.get('terminal')
+    self.filepath         = self.window.active_view().file_name()
+    self.filetype         = self.set_file_type()
+    self.testable_lines   = ["Run'em All"]
 
-    if self.check_for_appscript_installation():
-      settings              = sublime.load_settings('TestChooser.sublime-settings')
-      search_terms          = settings.get('search_terms')
-      terminal              = settings.get('terminal')
-      self.filepath         = self.view.file_name()
-      self.filetype         = self.set_file_type()
-      self.last_choice_path = self.ensure_last_choice_path()
+    if self.filetype == 'unknown':
+      self.exit_with_alert()
 
-      if self.filetype == 'unknown':
-        self.exit_with_alert()
+    else:
+      line_number = 0
 
+      for line in io.open(self.filepath, 'r'):
+        line_number += 1
+        for word in search_terms:
+          if line.strip().startswith(word):
+            self.testable_lines.append("%03d:%s" % (line_number, line))
+            break
+
+      chosen_test = self.choose_test()
+
+  def choose_test(self):
+    self.window.show_quick_panel(self.testable_lines, self.chose_item)
+
+  def chose_item(self, chosen_test):
+    if chosen_test != -1:
+      formatted_line = self.format_line(self.testable_lines[chosen_test])
+
+      if self.terminal == "iTerm":
+        applescript = self.iterm_script()
+        self.execute_cmd(applescript, formatted_line)
       else:
-        line_number = 0
-        testable_lines = ["Run'em All"]
+        applescript = self.terminal_script()
+        self.execute_cmd(applescript, formatted_line)
 
-        for line in io.open(self.filepath, 'r'):
-          line_number += 1
-          for word in search_terms:
-            if line.strip().startswith(word):
-              testable_lines.append("%03d:%s" % (line_number, line))
-              break
-
-        chosen_test = self.choose_test(testable_lines)
-        if chosen_test:
-          cmd = self.format_line(chosen_test)
-
-          if terminal == "":
-            self.execute_cmd(cmd)
-          else:
-            self.execute_cmd_in_terminal(cmd)
-
-  def choose_test(self, listitems):
-    sa = OSAX()
-    last_choice = self.get_last_choice()
-
-    if last_choice != '':
-      chosen_test = sa.choose_from_list(listitems,
-                                        with_title=u'Choose a test to run.',
-                                        default_items=[last_choice],
-                                        multiple_selections_allowed=False,
-                                        OK_button_name=u'Run')
-    else:
-      chosen_test = sa.choose_from_list(listitems,
-                                        with_title=u'Choose a test to run.',
-                                        multiple_selections_allowed=False,
-                                        OK_button_name=u'Run')
-
-    if chosen_test != False:
-      chosen = chosen_test[0]
-      self.set_last_choice(chosen)
-      return chosen
-    else:
-      return False
+    self.activate_sublime()
 
   def format_line(self, line):
     if line == "Run'em All":
-      return "bundle exec %s %s --drb" % (self.filetype, self.filepath)
+      return "%s '%s'" % (self.filetype, self.filepath)
     else:
       items = line.split(":")
-      return "bundle exec %s %s:%s --drb" % (self.filetype, self.filepath, items[0])
+      return "%s '%s:%s'" % (self.filetype, self.filepath, items[0])
 
-  def execute_cmd(self, cmd):
-    app(u'iTerm.app').terminals[1].current_session.write(text=cmd)
-
-  def execute_cmd_in_terminal(self,cmd):
-    app(u'Terminal.app').windows[1].do_script(cmd, in_=app.windows[1].selected_tab)
+  def execute_cmd(self, applescript, formatted_line):
+    cmd = applescript.replace("$cmd", formatted_line)
+    cmd = "osascript -e '%s'" % cmd
+    os.system(cmd)
 
   def set_file_type(self):
     if 'feature' in self.filepath:
-      return 'cucumber -r features/'
+      return self.settings.get('cucumber_command')
     elif 'spec' in self.filepath:
-      return 'rspec'
+      return self.settings.get('rspec_command')
     else:
       return 'unknown'
 
   def exit_with_alert(self):
-    OSAX().display_dialog('This is not a Cucumber or RSpec file.', buttons=['OK'], default_button=1)
+    sublime.error_message('This is not a Cucumber or RSpec file.')
 
-  def get_last_choice(self):
-    return io.open(self.last_choice_path, 'r').readline()
+  def iterm_script(self):
+    return """
+      tell application "iTerm"
+        tell current session of terminal 1
+          write text "$cmd"
+        end tell
+      end tell
+    """
 
-  def set_last_choice(self, choice):
-    f = open(self.last_choice_path, 'w')
-    f.write(choice)
-    f.close()
+  def terminal_script(self):
+    return """
+      tell application "Terminal"
+        tell window 1
+          do script "$cmd" in selected tab
+        end tell
+      end tell
+    """
 
-  def ensure_last_choice_path(self):
-    path = self.last_choice_file('last_choice.txt')
-    if not os.path.exists(path):
-      f = open(path, 'w')
-      f.write("Run'em All")
-      f.close()
-    return path
-
-  def last_choice_file(self, name):
-    return os.path.join(sublime.packages_path(), 'TestChooser', 'LastChoice', name)
-
-  def check_for_appscript_installation(self):
-    appscript_file_name = "appscript-1.0.0-py2.6-macosx-10.7-intel.egg"
-    dir_path = os.path.dirname(sublime.packages_path())
-    installed_packages_path = os.path.join(dir_path, appscript_file_name)
-    if not os.path.exists(installed_packages_path):
-      error_text = os.path.join(sublime.packages_path(), 'TestChooser', 'messages', 'install.txt')
-      self.window.open_file(error_text)
-      return False
-    else:
-      return True
+  def activate_sublime(self):
+    subprocess.Popen("""osascript -e 'tell app "Sublime Text 2" to activate'""", shell=True)
